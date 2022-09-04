890e3cbf258f02cf70a1a8eab1915022bf19f792
shedaniel <daniel@shedaniel.me>
2022-09-04 23:17:28 +0800

    Fix build

--- frontend/src/routes/Mappings.vue
+++ frontend/src/routes/Mappings.vue
@@ -8,13 +8,12 @@
             </div>
             <div class="col-[2/span_2] min-w-0">
                 <MappingsSearchBlock/>
-                <span>{{JSON.stringify(this.reqNamespacesPromise)}}</span>
                 <div v-if="infoData.entries.length > 0">
                     <MappingsEntryBlock v-for="entry in infoData.entries" :namespace="mappingsData.namespaces.find(value => value.id === infoData.namespace)"
                                         :translated-to-namespace="infoData.translateAs ? mappingsData.namespaces.find(value => value.id === infoData.translateAs) : undefined"
                                         :entry="entry" :version="version"/>
                 </div>
-                <div v-else-if="this.searchController" class="m-10 flex flex-col items-center">
+                <div v-else-if="searchController" class="m-10 flex flex-col items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search m-4" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <circle cx="10" cy="10" r="7"></circle>
