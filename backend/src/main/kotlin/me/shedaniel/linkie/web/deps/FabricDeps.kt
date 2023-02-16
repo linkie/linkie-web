2b9d61a2e9959aa0c09a2019f309ee16016a3c0e
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2023-02-16 16:55:49 +0100

    Add support for LegacyFabric Deps (#2)
    * Add support for LegacyFabric Deps
    
    * move common code to an abstract class

--- backend/src/main/kotlin/me/shedaniel/linkie/web/deps/FabricDeps.kt
+++ backend/src/main/kotlin/me/shedaniel/linkie/web/deps/FabricDeps.kt
@@ -1,62 +1,22 @@
 package me.shedaniel.linkie.web.deps
 
-import io.ktor.client.call.*
-import io.ktor.client.call.body
-import io.ktor.client.request.*
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.jsonArray
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
-import me.shedaniel.linkie.utils.info
-import me.shedaniel.linkie.utils.tryToVersion
-import me.shedaniel.linkie.web.Scopes
 import me.shedaniel.linkie.web.curseforge.CurseForgeRequests
 import me.shedaniel.linkie.web.curseforge.ModFile
 import me.shedaniel.linkie.web.curseforge.Pagination
-import me.shedaniel.linkie.web.httpClient
-import kotlin.math.ceil
 
-object FabricDeps : Deps("Fabric") {
-    override suspend fun provideData(): Map<VersionIdentifier, Data> = coroutineScope {
-        val dependencies = mutableMapOf<VersionIdentifier, MutableList<Dependency>>()
-        val metaDeferred = async { httpClient.get("https://meta.fabricmc.net/v2/versions").body<JsonObject>() }
-        val pagination = async { CurseForgeRequests.getModFiles(306612, pageSize = 1).pagination }
-        val meta = metaDeferred.await()
-        val loaderVersion = meta["loader"]!!.jsonArray[0].jsonObject["version"]!!.jsonPrimitive.content
-        val mappings = meta["mappings"]!!.jsonArray
-        meta["game"]!!.jsonArray.asSequence().map(JsonElement::jsonObject).forEach { obj ->
-            val version = obj["version"]!!.jsonPrimitive.content
-            val release = version.tryToVersion().let { it != null && it.snapshot == null }
-            val mappingsObj = mappings.firstOrNull { it.jsonObject["gameVersion"]!!.jsonPrimitive.content == version }?.jsonObject ?: return@forEach
-            val yarnVersion = mappingsObj["version"]!!.jsonPrimitive.content
-            val versionIdentifier = VersionIdentifier(
-                loader = "fabric",
-                version = version,
-                stable = release,
-            )
-            dependencies.getOrPut(versionIdentifier, ::mutableListOf).apply {
-                add(Dependency("Yarn", DependencyType.Mappings, "net.fabricmc:yarn:$yarnVersion:v2", yarnVersion))
-                add(Dependency("Fabric Loader", DependencyType.Implementation, "net.fabricmc:fabric-loader:$loaderVersion", loaderVersion))
-            }
-        }
-        fillFabricApi(pagination.await(), dependencies)
-        return@coroutineScope dependencies.mapValues {
-            Data(mavens = listOf(), dependencies = it.value)
-        }
-    }
-
-    private suspend fun fillFabricApi(pagination: Pagination, versions: MutableMap<VersionIdentifier, MutableList<Dependency>>) {
+object FabricDeps : AbstractFabricDeps(
+    "Fabric",
+    "https://meta.fabricmc.net/v2/versions",
+    306612,
+    "Yarn",
+    "net.fabricmc:yarn"
+) {
+    override suspend fun fillFabricApi(pagination: Pagination, versions: MutableMap<VersionIdentifier, MutableList<Dependency>>) {
         val displayNameRegex = "\\[(.*)].*".toRegex()
         val fileNameRegex = "fabric(?:-api)?-(.*).jar".toRegex()
         val versionSplitRegex = "([^/]+)(./.*)".toRegex()
         val addonFiles: MutableMap<String, MutableList<ModFile>> = mutableMapOf()
-        val files = CurseForgeRequests.getAllModFiles(306612, pagination)
+        val files = CurseForgeRequests.getAllModFiles(this.curseforgeId, pagination)
         files.forEach { file ->
             val displayedVersion = displayNameRegex.matchEntire(file.displayName)?.groupValues?.get(1) ?: return@forEach
             val splitMatchResult = versionSplitRegex.matchEntire(displayedVersion)
---
+++ backend/src/main/kotlin/me/shedaniel/linkie/web/deps/LegacyFabricDeps.kt
@@ -0,0 +1,39 @@
+package me.shedaniel.linkie.web.deps
+
+import me.shedaniel.linkie.web.curseforge.CurseForgeRequests
+import me.shedaniel.linkie.web.curseforge.ModFile
+import me.shedaniel.linkie.web.curseforge.Pagination
+
+object LegacyFabricDeps : AbstractFabricDeps(
+    "Legacy Fabric",
+    "https://meta.legacyfabric.net/v2/versions",
+    400281,
+    "Legacy Yarn",
+    "net.legacyfabric:yarn",
+    listOf(MavenData(
+        subtitle = "Legacy Fabric",
+        "https://repo.legacyfabric.net/repository/legacyfabric/"
+    ))
+) {
+    override suspend fun fillFabricApi(pagination: Pagination, versions: MutableMap<VersionIdentifier, MutableList<Dependency>>) {
+        val fileNameRegex = "Legacy Fabric API (.*)".toRegex()
+        val addonFiles: MutableMap<String, MutableList<ModFile>> = mutableMapOf()
+        val files = CurseForgeRequests.getAllModFiles(this.curseforgeId, pagination)
+        files.forEach { file ->
+            val displayedVersion = fileNameRegex.matchEntire(file.displayName)
+            if (displayedVersion != null) {
+                file.gameVersions.forEach { gameV ->
+                    addonFiles.getOrPut(gameV, ::mutableListOf).add(file)
+                }
+            }
+        }
+        versions.forEach versionLoop@{ versionIdentifier, dependencies ->
+            val match = addonFiles[versionIdentifier.version]?.toMutableList()?.sortedByDescending(ModFile::id)?.first()
+            if (match != null) {
+                val version = fileNameRegex.matchEntire(match.displayName)!!.groupValues[1]
+                dependencies.add(Dependency("Legacy Fabric API", DependencyType.Implementation,
+                    "net.legacyfabric.legacy-fabric-api:legacy-fabric-api:$version+${versionIdentifier.version}", version))
+            }
+        }
+    }
+}
