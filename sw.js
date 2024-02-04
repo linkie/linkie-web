357a0810b8b5303ede91242526fcdee8a9a2fd34
shedaniel <daniel@shedaniel.me>
2024-02-04 14:35:25 +0000

    Deploying to gh-pages from @ linkie/linkie-web@db002c29b038191e0920b942d51c576a120360f8 🚀

--- sw.js
+++ sw.js
@@ -1,2 +1,14 @@
-if(!self.define){let e,i={};const n=(n,r)=>(n=new URL(n+".js",r).href,i[n]||new Promise((i=>{if("document"in self){const e=document.createElement("script");e.src=n,e.onload=i,document.head.appendChild(e)}else e=n,importScripts(n),i()})).then((()=>{let e=i[n];if(!e)throw new Error(`Module ${n} didn’t register its module`);return e})));self.define=(r,c)=>{const d=e||("document"in self?document.currentScript.src:"")||location.href;if(i[d])return;let s={};const o=e=>n(e,d),f={module:{uri:d},exports:s,require:o};i[d]=Promise.all(r.map((e=>f[e]||o(e)))).then((e=>(c(...e),s)))}}define(["./workbox-3ea082d2"],(function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"404.html",revision:"2f4750e6e1fce770a0bf29efee8411a8"},{url:"assets/index.48a6a127.js",revision:null},{url:"assets/index.f563a286.css",revision:null},{url:"index.html",revision:"608bccbf1dd4bc3d8be4c4ac78a8efd2"},{url:"registerSW.js",revision:"1872c500de691dce40960bb85481de07"},{url:"favicon.ico",revision:"47ab46e1d952a9d40272c7f0cd35ee5a"},{url:"favicon-32x32.png",revision:"6266c2b94d99a0d34de5646847edbbb1"},{url:"favicon-16x16.png",revision:"8eb8b30dcd184b16c6f6418fd3a9cf35"},{url:"android-chrome-192x192.png",revision:"0fff8436bfb4adc190c81e4e9c2b5e91"},{url:"android-chrome-512x512.png",revision:"d01ace399db5fadd95a2adabcdc8642a"},{url:"apple-touch-icon.png",revision:"7d6395799758181d4575407e0498442c"},{url:"safari-pinned-tab.svg",revision:"c0622a66c58b97e1837b2b3605f97da0"},{url:"robots.txt",revision:"f77c87f977e0fcce05a6df46c885a129"},{url:"manifest.webmanifest",revision:"45ff19081b0817e0485f0dee272ae612"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))}));
-//# sourceMappingURL=sw.js.map
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
+});
+    
